1f9b51290cb7fc588260429c73083428df3c1f74
mauliksoni110 <happy.maulik@yahoo.co.in>
2015-08-12 15:03:34 +0100

    Section 2 added

--- js/main.js
+++ js/main.js
@@ -18,13 +18,15 @@ $j(window).load(function() {
 
                                         //SCROLLMAGIC SETTINGS STARTS
 
-//Dynamic Values
-var section_1_Height = parseInt($j('.section1 .sectionInner.boxes-wrapper').outerHeight()) + 100;
+                                        //Dynamic Values
+var R_SECTION_1_HEIGHT = parseInt($j('.section1 .sectionInner.boxes-wrapper').outerHeight()) + 100;
+var R_HEADER_HEIGHT = 50;
 
 
 var smController = new ScrollMagic.Controller();
 
-                  /****************SECTION 2 STARTS****************/
+
+                  /****************SECTION 1 STARTS****************/
 var boxesTween = new TweenMax.staggerTo('.Content-wrapper .section1 .box-item', 0.4, {
 "transform":"translate('0px','0px')",
 "opacity":"1"
@@ -40,7 +42,7 @@ var headerTween = new TweenMax.to('.navbar-fixed-top', 0.2, {
 });
 
 
-var sceneHeader = new ScrollMagic.Scene({offset:1,triggerHook:"onLeave"
+var sceneHeader = new ScrollMagic.Scene({offset:1,triggerHook:"onLeave",duration:40
 })
 .setTween(headerTween)
 .addTo(smController);
@@ -48,13 +50,13 @@ var sceneHeader = new ScrollMagic.Scene({offset:1,triggerHook:"onLeave"
 
 
 
-var sceneSection1 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section1',offset:section_1_Height,triggerHook:'onEnter'
+var sceneSection1 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section1',offset:R_SECTION_1_HEIGHT,triggerHook:'onEnter'
 })
 .setPin('.Content-wrapper .section.section1')
 .addTo(smController);
 //.addIndicators();
 
-                  /****************SECTION 1 STARTS****************/
+                  /****************SECTION 1 Ends****************/
 
 
                   /****************SECTION 2 STARTS****************/
@@ -69,45 +71,71 @@ var sceneSection2header = new ScrollMagic.Scene({triggerElement:'.Content-wrappe
 
 
 ///// PART1
-var timeLine1 = new TimelineMax();
+var timeLinePart1 = new TimelineMax();
 var partTween1 = TweenMax.to('.part.part-1 .part-content',0.3,{'left':0});
 var partTween1Img = TweenMax.to('.part.part-1 img',0.3,{'right':0});
-timeLine1.add([partTween1,partTween1Img]);
+timeLinePart1.add([partTween1,partTween1Img]);
 
 var sceneSection2part1 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section2 .part-1',duration:400,offset:-300
 })
-.setTween(timeLine1)
+.setTween(timeLinePart1)
 .addTo(smController);
 //.addIndicators();
 
 
 ///// PART2
-var timeLine2 = new TimelineMax();
+var timeLinePart2 = new TimelineMax();
 var partTween2 = TweenMax.to('.part.part-2 .part-content',0.3,{'right':0});
 var partTween2Img = TweenMax.to('.part.part-2 img',0.3,{'left':0});
-timeLine2.add([partTween2,partTween2Img]);
+timeLinePart2.add([partTween2,partTween2Img]);
 
-var sceneSection2part2 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section2 .part-2',duration:400,offset:-300
+var sceneSection2part2 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section2 .part-2',duration:400,offset:-400
 })
-.setTween(timeLine2)
+.setTween(timeLinePart2)
 .addTo(smController);
 //.addIndicators();
 
 ////PART3
 
-var timeLine3 = new TimelineMax();
+var timeLinePart3 = new TimelineMax();
 var partTween3 = TweenMax.to('.part.part-3 .part-content',0.3,{'left':0});
 var partTween3Img = TweenMax.to('.part.part-3 img',0.3,{'right':0});
-timeLine3.add([partTween3,partTween3Img]);
+timeLinePart3.add([partTween3,partTween3Img]);
 
 var sceneSection2part3 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section2 .part-3',duration:400,offset:-300
 })
-.setTween(timeLine3)
+.setTween(timeLinePart3)
 .addTo(smController);
 //.addIndicators();
                         /****************SECTION 2 ENDS****************/
 
+
+
+
                         /****************SECTION 3 STARTS****************/
+var timeLineSection3_initShow = new TimelineMax();
+var sceneSection3_initShow_title = TweenMax.to('.init-show-title',0.6,{"delay":"0.1","transform":"translateY(-300px)"});
+var sceneSection3_initShow_img = TweenMax.to('.init-show-image',0.6,{"delay":"0.1","transform":"translateY(2000px)"});
+var sceneSection3_main_content = TweenMax.to('.section-3-main-content',0.8,{"delay":"0.2","opacity":"1"});
+
+//var sceneSection3_main_content_panel = new TweenMax.staggerTo('.section-3-main-content .panel',0.8,{"top":"0px","opacity":"1"},0.3);
+
+var sceneSection3_main_content_header = TweenMax.to('.section-3-main-content .section-3-main-content-header',0.2,{"delay":"0.2","top":"0",
+onComplete:function(){TweenMax.staggerTo('.section-3-main-content .panel',0.8,{"top":"0px","opacity":"1"},0.3);},
+onReverseComplete:function(){TweenMax.staggerTo('.section-3-main-content .panel',0.2,{"top":"70px","opacity":"0"},0.1);}
+});
+
+
+
+timeLineSection3_initShow.add([sceneSection3_initShow_title,sceneSection3_initShow_img,sceneSection3_main_content,sceneSection3_main_content_header]);
+
+var sceneSection3 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section3',duration:"150%",triggerHook:'onLeave',offset:-R_HEADER_HEIGHT
+})
+.setPin('.Content-wrapper .section.section3')
+.setTween(timeLineSection3_initShow)
+.addTo(smController)
+.addIndicators();
+
 
 
 

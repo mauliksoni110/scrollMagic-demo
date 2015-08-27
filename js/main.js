b23b0586e6c8aabd74531743981cd4ff2fe47bd9
mauliksoni110 <happy.maulik@yahoo.co.in>
2015-08-27 12:43:09 +0100

    new changes

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ function bottomBoxesAnim() {
 //Background image parallex Starts
 var section1_background_tween = TweenMax.to('.section.section-1',0.5,{'background-position':'50% '+R_VIEWPORT.height/2+'px'});
 
-var sceneSection1_background = new ScrollMagic.Scene({triggerHook:"onLeave",duration:"100%"
+var sceneSection1_background_scene = new ScrollMagic.Scene({triggerHook:"onLeave",duration:"100%"
 })
 .setTween(section1_background_tween)
 .addTo(smController);
@@ -101,11 +101,11 @@ section1_bottomBoxesPositionSet = function(){
 
 /****************SECTION 2 STARTS****************/
 //// Section 2 HEADER
-var section2headerTween = TweenMax.from('.Content-wrapper .section.section-2 h2',1,{'top':300,'opacity':0});
+var section2header_tween = TweenMax.from('.Content-wrapper .section.section-2 h2',1,{'top':300,'opacity':0});
 
-var sceneSection2header = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-2',offset:200,duration:200,triggerHook:'onEnter'
+var sceneSection2header_scene = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-2',offset:200,duration:200,triggerHook:'onEnter'
 })
-.setTween(section2headerTween)
+.setTween(section2header_tween)
 .addTo(smController);
 
 
@@ -117,7 +117,7 @@ $j('.section-2 .part').each(function(){
   else
     partTween = TweenMax.from($j(this),0.4,{'left':'-120%','opacity':0});
 
-  var sceneSection2part1 = new ScrollMagic.Scene({triggerElement:$j(this),offset:-300
+  var section2part1_scene = new ScrollMagic.Scene({triggerElement:$j(this),offset:-300
   })
   .setTween(partTween)
   .addTo(smController);
@@ -128,16 +128,16 @@ $j('.section-2 .part').each(function(){
 
 
 /****************SECTION 3 STARTS****************/
-var timeLineSection3_initShow = new TimelineMax();
-var tweenSection3_initShow_title = TweenMax.to('.init-show-title',2,{"delay":"1","transform":"translateY(-300px)"});
-var tweenSection3_initShow_img = TweenMax.to('.init-show-image',2,{"delay":"1","transform":"translateY(2000px)"});
-var tweenSection3_main_content = TweenMax.to('.section-3-main-content',0.8,{"delay":"1","opacity":"1"});
-var tweenSection3_main_content_header = TweenMax.from('.section-3-main-content .section-3-main-content-header',0.6,{"delay":"2","top":"-40",
-onComplete:onComplete_tweenSection3_main_content_header,
-onReverseComplete:onReverseComplete_tweenSection3_main_content_header
+var section3_initShow_timeline = new TimelineMax();
+var section3_initShow_title_tween = TweenMax.to('.init-show-title',2,{"delay":"1","transform":"translateY(-300px)"});
+var section3_initShow_img_tween = TweenMax.to('.init-show-image',2,{"delay":"1","transform":"translateY(2000px)"});
+var section3_main_content_tween = TweenMax.to('.section-3-main-content',0.8,{"delay":"1","opacity":"1"});
+var section3_main_content_header_tween = TweenMax.from('.section-3-main-content .section-3-main-content-header',0.6,{"delay":"2","top":"-40",
+onComplete:onComplete_section3_main_content_tween_header,
+onReverseComplete:onReverseComplete_section3_main_content_tween_header
 });
 
-function onComplete_tweenSection3_main_content_header(){
+function onComplete_section3_main_content_tween_header(){
   TweenMax.staggerTo('.section-3-main-content .panel',0.6,{"top":"0px","opacity":"1"},0.1);
   TweenMax.to('.section-3-counters-wrapper',0.6,{"opacity":"1"});
   if(!counter_1_done){
@@ -145,45 +145,44 @@ function onComplete_tweenSection3_main_content_header(){
     counter_1_done = true;
   }
 }
-function onReverseComplete_tweenSection3_main_content_header(){
+function onReverseComplete_section3_main_content_tween_header(){
   TweenMax.staggerTo('.section-3-main-content .panel',0.2,{"top":"70px","opacity":"0"},0.1);
   TweenMax.to('.section-3-counters-wrapper',0.2,{"opacity":"0"});
 }
 
 
-timeLineSection3_initShow.add([tweenSection3_initShow_title,tweenSection3_initShow_img,tweenSection3_main_content,tweenSection3_main_content_header]);
+section3_initShow_timeline.add([section3_initShow_title_tween,section3_initShow_img_tween,section3_main_content_tween,section3_main_content_header_tween]);
 
 var sceneSection3 = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-3', duration:"1300", triggerHook:'onLeave', offset:-R_HEADER_HEIGHT
 })
 .setPin('.Content-wrapper .section.section-3')
-.setTween(timeLineSection3_initShow)
+.setTween(section3_initShow_timeline)
 .addTo(smController);
-
 /****************SECTION 3 ENDS****************/
 
 
 /****************SECTION 4 STARTS****************/
 
-var Section4_table_tween = TweenMax.staggerFrom('.section-4-stagger-rows-wrapper .stagger-row',0.6,{y:300,'opacity':0},0.3);
+var section4_table_tween = TweenMax.staggerFrom('.section-4-stagger-rows-wrapper .stagger-row',0.6,{y:300,'opacity':0},0.3);
 
 var section4_scene = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-4', duration:"150%", triggerHook:'onLeave', offset:-R_HEADER_HEIGHT,
 })
 .setPin('.Content-wrapper .section.section-4')
-.setTween(Section4_table_tween)
+.setTween(section4_table_tween)
 .addTo(smController);
-
-var section4_content_scene = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-4', triggerHook:'onLeave', offset:-R_HEADER_HEIGHT})
-.setPin('.section-4-inner-wrapper')
-.addTo(smController);
+// var section4_content_scene = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-4', triggerHook:'onLeave', offset:-R_HEADER_HEIGHT})
+// .setPin('.section-4-inner-wrapper')
+// .addTo(smController);
 /****************SECTION 4 ENDS****************/
 
 
 /****************SECTION 5 STARTS****************/
-var Section5_flips_tween = TweenMax.staggerFromTo('.section-5-main-content-wrapper .flips-wrapper .flip-container .flipper',0.6,{transform:'rotateX(-180deg)','opacity':0},{transform:'rotateX(0deg)','opacity':1,onComplete:function(){
-  $j('.section-5-main-content-wrapper .flips-wrapper .flip-container .flipper').addClass('flip-enabled');
-},onReverseComplete:function(){
+var onAllComplete = function(){$j('.section-5-main-content-wrapper .flips-wrapper .flip-container .flipper').addClass('flip-enabled');};
+
+var Section5_flips_tween = TweenMax.staggerFromTo('.section-5-main-content-wrapper .flips-wrapper .flip-container .flipper',0.4,{transform:'rotateX(-90deg)','opacity':0},{transform:'rotateX(0deg)','opacity':1,onReverseComplete:function(){
   $j('.section-5-main-content-wrapper .flips-wrapper .flip-container .flipper').removeClass('flip-enabled');
-}},0.3);
+  }
+},0.2,onAllComplete);
 
 var section5_content_scene = new ScrollMagic.Scene({triggerElement:'.Content-wrapper .section.section-5', triggerHook:'onLeave', offset:-R_HEADER_HEIGHT,reverse:false})
 .setTween(Section5_flips_tween)
@@ -201,12 +200,4 @@ section5_content_scene.on('start',function(e){
 });
 
 
-
-function viewport() {
-    var e = window, a = 'inner';
-    if (!('innerWidth' in window )) {
-        a = 'client';
-        e = document.documentElement || document.body;
-    }
-    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-}
+function viewport() {var e = window, a = 'inner';if (!('innerWidth' in window )) {a = 'client';e = document.documentElement || document.body;}return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };}
